444db0d8af7609879199a2db9bada71db56f77a7
tzipor <50782805+tzipor@users.noreply.github.com>
2022-01-02 01:12:14 -0500

    Implemented del.go, but need to modify jobert-api to allow DELETE requests

--- cmd/del.go
+++ cmd/del.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -10,33 +13,53 @@ import (
 // delCmd represents the del command
 var delCmd = &cobra.Command{
 	Use:   "del",
-	Short: "Delete queries or jobs from database",
-	Long: `Delete queries or jobs from API database.
+	Short: "Delete queries from database",
+	Long: `Delete queries from API database.
 Use the -q/--qid flag to filter by query_id (required).
 
 Deleting all entries is not supported by the API.
 
-Proper usage: jobert del [query/job] -q #`,
+Proper usage: jobert del query -q #`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) != 1 {
 			return errors.New("argument 'query' or 'job' required")
 		}
-		if args[0] == "query" || args[0] == "job" {
+		if args[0] == "query" {
 			return nil
 		}
 		return fmt.Errorf("invalid command 'jobert del %s'", args[0])
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("del called")
-		// TODO
+		if qid == -1 {
+			log.Fatal("-q/--qid is required.")
+		}
+
+		client := &http.Client{}
+
+		url := fmt.Sprintf("http://localhost:8080/query?id=%d", qid)
+		req, err := http.NewRequest("DELETE", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//
+		fmt.Println(resp.Status)
+		//fmt.Println(resp.Header)
+		fmt.Sprintln(string(b))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(delCmd)
 }
-
-// TODO (note deleting all rows is not supported by the API)
-// 1. confirm qid != -1
-// 2. Send DELETE request to /job or /query for qid
-// 3. Print statement based on response
